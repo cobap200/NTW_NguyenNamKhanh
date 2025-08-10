d01ce8fb9d94b020a965a7ff673f663857ab8820
Nguyễn Nam Khánh <61370964+cobap200@users.noreply.github.com>
2025-08-10 09:34:23 +0700

    Add files via upload

---
+++ buoi8/js/data.js
@@ -0,0 +1,37 @@
+const danhSachGiaoDich = [
+  {
+    id: 2001,
+    khachHang: "Lý Minh Tuấn",
+    nhanVien: "Nguyễn Thị Hoa",
+    soTien: "2,300,000",
+    ngayMua: "10/08/2025, 09:15:00"
+  },
+  {
+    id: 2002,
+    khachHang: "Phạm Quang Huy",
+    nhanVien: "Trần Văn Khánh",
+    soTien: "1,750,000",
+    ngayMua: "08/08/2025, 14:20:00"
+  },
+  {
+    id: 2003,
+    khachHang: "Nguyễn Thị Mai",
+    nhanVien: "Lê Văn Dũng",
+    soTien: "950,000",
+    ngayMua: "11/08/2025, 16:45:00"
+  },
+  {
+    id: 2004,
+    khachHang: "Trần Gia Bảo",
+    nhanVien: "Nguyễn Thị Hoa",
+    soTien: "3,500,000",
+    ngayMua: "12/08/2025, 11:10:00"
+  },
+  {
+    id: 2005,
+    khachHang: "Võ Thị Lan",
+    nhanVien: "Trần Văn Khánh",
+    soTien: "1,200,000",
+    ngayMua: "09/08/2025, 13:05:00"
+  }
+];
